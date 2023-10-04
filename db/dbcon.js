dfa109acdd775100e497a10c3600dc1a761163df
Shikhar Saxena <shikhars74@gmail.com>
2023-10-04 19:17:59 +0530

    fourth commit

--- db/dbcon.js
+++ db/dbcon.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const local_url = 'mongodb://127.0.0.1:27017/BlogProject';
-const live_url = 'mongodb+srv://shikhars74:shikhar123@blogcluster.a378mpj.mongodb.net/?retryWrites=true&w=majority';
+const live_url = 'mongodb+srv://shikhars74:shikhar123@admissioncluster.a378mpj.mongodb.net/?retryWrites=true&w=majority';
 
 const condb = ()=>{
 return mongoose.connect(live_url)
